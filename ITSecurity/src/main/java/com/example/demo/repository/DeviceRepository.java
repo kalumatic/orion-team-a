e8e08bb104abb3709b8d01dd65a052bed6c2b1eb
Sonja Vukojicic <sonja.vukojicic@orioninc.com>
2026-02-24 20:50:29 +0100

    Use EntityGraph to optimize Retrieve Devices

--- ITSecurity/src/main/java/com/example/demo/repository/DeviceRepository.java
+++ ITSecurity/src/main/java/com/example/demo/repository/DeviceRepository.java
@@ -1,6 +1,10 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.Device;
+import lombok.NonNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +16,9 @@ public interface DeviceRepository extends JpaRepository<Device,Long> {
     List<Device> findByDeviceType(String deviceType);
 
     Optional<Device> findBySerialNumber(String serialNumber);
+
+    @Override
+    @NonNull
+    @EntityGraph(attributePaths = {"assignedEmployee"})
+    Page<Device> findAll(@NonNull Pageable pageable);
 }
