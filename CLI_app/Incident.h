406b382bba8574ed7b5780efe354533a5875e7a4
Petar Magenhajm <petar.magenhajm@mera.com>
2026-02-24 23:54:08 +0100

    beggining of the implemetation of Incident

---
+++ CLI_app/Incident.h
@@ -0,0 +1,40 @@
+#pragma once
+#pragma once
+
+#include <string>
+#include <chrono>
+#include "Employee.h"
+
+class Incident
+{
+public:
+    enum class Severity
+    {
+        Low,
+        Medium,
+        High,
+        Critical
+    };
+
+    enum class Status
+    {
+        Open,
+        InProgress,
+        Closed
+    };
+
+    Incident(const Employee& reporter,
+        const std::string& device,
+        const std::string& description,
+        Severity severity);
+private:
+    static bool isValid(Employee e, std::string d);
+
+private:
+    Employee m_reporter;
+    std::string m_device;
+    std::string m_description;
+    std::chrono::system_clock::time_point m_date;
+    Severity m_severity;
+    Status m_status;
+};
