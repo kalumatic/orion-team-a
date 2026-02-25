73419973c8030d13031125c5102be5d3db9d2021
Luka Matic <luka.matic@mera.com>
2026-02-25 12:53:52 +0100

    add /bulk post mapping to accept bulk device requests

--- ITSecurity/src/main/java/com/example/demo/controller/DeviceController.java
+++ ITSecurity/src/main/java/com/example/demo/controller/DeviceController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.BulkDeviceInsertResponseDTO;
 import com.example.demo.dto.DeviceRequestDTO;
 import com.example.demo.dto.DeviceResponseDTO;
 import com.example.demo.service.DeviceService;
@@ -10,6 +11,8 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/devices")
@@ -24,6 +27,12 @@ public class DeviceController {
     public ResponseEntity<DeviceResponseDTO> create(@Valid @RequestBody DeviceRequestDTO request) {
         DeviceResponseDTO response = service.createDevice(request);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/bulk")
+    public ResponseEntity<BulkDeviceInsertResponseDTO> createBulk(@RequestBody List<DeviceRequestDTO> requests) {
+        BulkDeviceInsertResponseDTO response = service.createDevicesBulk(requests);
+        return ResponseEntity.ok(response);
     }
 
     @PutMapping("/{id}")
